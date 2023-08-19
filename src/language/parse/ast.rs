06b02f7b31775894635c7c7898ce4c130fffe17d
Varun Ramani <varun.ramani@gmail.com>
2023-08-19 16:17:22 -0400

    wrote basic AST

---
+++ src/language/parse/ast.rs
@@ -0,0 +1,29 @@
+//! The definition of the abstract syntax tree (AST) lives 
+//! inside this file. 
+
+
+#[derive(Debug)]
+/// The root node; all blockpipe programs are actually just expressions within
+/// expressions.
+pub enum Expression {
+    Block(Vec<Expression>),
+    Pipe(Box<Expression>, PipeType, Box<Expression>),
+    Tuple(Vec<Expression>),
+    Literal(LiteralType)
+}
+
+/// Blockpipe supports string, integer, and float literals.
+#[derive(Debug)]
+pub enum LiteralType {
+    Str(String),
+    Int(i64),
+    Float(f64),
+}
+
+/// If you want to pass multiple arguments to a closure, you'll have to pass
+/// them as a tuple through a destructure pipe.
+#[derive(Debug)]
+pub enum PipeType {
+    Destructure,
+    Flow
+}
