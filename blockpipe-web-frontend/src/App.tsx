80741b3c1822f7d022120a0c49c3dc029a561cfe
Varun Ramani <varun.ramani@gmail.com>
2023-12-30 17:03:11 -0500

    finished website

--- blockpipe-web-frontend/src/App.tsx
+++ blockpipe-web-frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon, Center, Container, Divider, Group, Stack, Text, Title } from "@mantine/core";
+import { ActionIcon, Anchor, Center, Container, Divider, Group, Stack, Text, Title } from "@mantine/core";
 import { IconBrandGithub, IconUser } from "@tabler/icons-react";
 import BlockpipeEditor from "./editor";
 import tutorialSections from './tut.yml';
@@ -30,7 +30,7 @@ export default function App() {
                                 <IconBrandGithub />
                             </ActionIcon>
                         </Stack>
-                        <Title p={10}>|</Title>
+                        <Title p={10}>{`()|{}`}</Title>
                         <Stack>
                             <Title order={2}>An Interactive Tour of BlockPipe</Title>
                             <Title order={4}>A functional language built around the pipe operator</Title>
@@ -48,6 +48,36 @@ export default function App() {
                     console.log(value);
                     return <TutorialSection startingCode={value['startingCode']} expectedCode={value['expect']} title={value['title']} content={value['content']} key={idx} />
                 })}
+
+                <Divider my={20} />
+                
+                <Stack>
+                    <Title order={4}>
+                        You made it! That was a whirlwind tour of BlockPipe. What next?
+                    </Title>
+
+                    <Text>
+                        I'd like to extend my sincere appreciation for your time. I hope that if you made it this far, you found BlockPipe 
+                        at least somewhat interesting. A lot of hard work went into this project, and it's definitely one of the coolest things 
+                        I've made. 
+                    </Text>
+
+                    <Text>
+                        Furthermore, if you're curious, there's a lot more you can explore. This webpage is backed by a small component of the BlockPipe interpreter
+                        that I compiled to WebAssembly, but the language is actually implemented in Rust and can be executed locally. There's also an 
+                        in-development compiler targeting LLVM. 
+                    </Text>
+
+                    <Text>
+                        If you have some more time, you could take a look at the <Anchor href="https://github.com/varun-ramani/blockpipe">BlockPipe source code</Anchor>. 
+                        Or, if you want to learn about some of the other things I've worked on, you could <Anchor href="https://varunramani.com">check out my portfolio.</Anchor> 
+                    </Text>
+
+                    <Text>
+                        Thanks again, <br />
+                        Varun Ramani
+                    </Text>
+                </Stack>
 
             </Container>
         </Stack>
