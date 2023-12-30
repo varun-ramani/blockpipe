e55b937808d98b7cc03822cd20ffa24a7315be7b
Varun Ramani <varun.ramani@gmail.com>
2023-12-29 19:03:50 -0500

    added wasm entry point

--- language/src/lib.rs
+++ language/src/lib.rs
@@ -6,6 +6,7 @@ use interpreter::{EvaluateResult, Interpreter, Value};
 use lexer::Token;
 use logos::{Logos, Span};
 use parser::{ASTNode, ParseResult, Parser, ParserError};
+use wasm_bindgen::prelude::*;
 
 pub fn lex_from_string(input: &str) -> Vec<(Result<Token, ()>, Span)> {
     Token::lexer(input).spanned().collect()
@@ -46,5 +47,27 @@ pub fn interpret_from_string(
         Ok(Interpreter::execute_closure(parameters_vector, &res)?)
     } else {
         Ok(res)
+    }
+}
+
+#[wasm_bindgen]
+extern {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn wasm_interpret_from_string(
+    input: &str,
+    parameters: Option<Vec<String>>,
+    execute_root: bool
+) -> String {
+    let res = interpret_from_string(input, parameters, execute_root);
+    match res {
+        Ok(ret_value) => {
+            ret_value.to_string()
+        },
+        Err(some_err) => {
+            some_err.to_string()
+        }
     }
 }
